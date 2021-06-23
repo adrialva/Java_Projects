0054914ea3add3e1261c2d685c74aeb29e617036
adrialva <58445399+adrialva@users.noreply.github.com>
2021-06-23 11:30:26 -0500

    Add files via upload

---
+++ carrera/src/carrera/Carrera.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrera;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ADRIANA
+ */
+public class Carrera {
+
+
+static float a=new float[5];
+
+static void menu(){
+    Scanner input = new Scanner (System.in);
+    System.out.println( "    **Bienvenido al programa de la competencia de caballos**");
+	}
+static void inicio(){
+
+    Scanner input = new Scanner (System.in);
+for(int i=0; i<5;i++){//entrar digitos
+        System.out.println("  Digite el puntaje obtenido de cada caballo"
+                +": ");
+		a[i]=input.nextInt();
+	}
+}
+static int proceso(){
+    if (a[1]<
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        menu();
+inicio();
+}
+    
+}
